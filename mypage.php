15f83c7f2f6bc95e6140c00a5af597b1ec48c1b4
FUAD HASAN <66431113+Fuad-h@users.noreply.github.com>
2021-05-25 21:23:22 +0600

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Form</h1>
+
+<form>
+First name: <input type='text' > <br> <br>
+Last name: <input type='text' > <br> <br>
+
+Gender: <br>
+Male <input type='radio' name ='gender'>
+Female <input type='radio' name ='gender'> <br>
+
+<p> Date:</p> 
+<input type ='date'> <br><br>
+
+<input type='Submit' value ='Submit'>
+
+</form>
+
+</body>
+</html>
